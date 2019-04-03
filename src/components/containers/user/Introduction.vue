d1d9a0c241aea02f9a8936c7c46c7eaf6d7b8fde
andy <andy2472625@gmail.com>
2019-04-03 16:40:14 +0800

    style: modify introduction layout

--- src/components/containers/user/Introduction.vue
+++ src/components/containers/user/Introduction.vue
@@ -5,10 +5,10 @@
         .cell
         .glasses
         .stars
-      .introduction__main.text-center
+      .introduction__main.text-center.footerSpace
         h3.text-primary 發票一目瞭然
         p.text 完成設定，信用卡就可以存發票！
-      .introduction__footer.columns
+      .section__footer.columns
         button.btn.btn-submit.column.col-12(
           @click="onClickHandler"
         ) 下一步
@@ -17,13 +17,13 @@
         .id
         .credit
         .stars.stars-v2
-      .introduction__main.text-center
+      .introduction__main.text-center.footerSpace
         h3.text-primary 我要怎麼做？
         p.text
           |申請手機條碼與綁定載具
           br
           |比你想像更簡單！
-      .introduction__footer.columns
+      .section__footer.columns
         button.btn.btn-submit.column.col-12(
           @click="onClickHandler"
         ) 下一步
@@ -35,13 +35,13 @@
           .creditgroup__card2.bg
           .creditgroup__card3.bg
         .stars.stars-v3
-      .introduction__main.text-center
+      .introduction__main.text-center.footerSpace
         h3.text-primary 綁定信用卡當載具
         p.text
           |發票存在信用卡
           br
           |妥善保存好環保
-      .introduction__footer.columns
+      .section__footer.columns
         button.btn.btn-submit.column.col-12(
           @click="onClickHandler"
         ) 開始綁定
@@ -220,15 +220,6 @@ export default {
 
   h3 {
     margin-bottom: convertUnit(10);
-  }
-
-  &__footer {
-    position: fixed;
-    width: calc(100% - #{$space * 2});
-    left: $space;
-    bottom: $space;
-    margin: 0;
-    z-index: 1;
   }
 }
 </style>
