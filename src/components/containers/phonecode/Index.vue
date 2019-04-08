0496b45c14acd33f53d8a4ab9c40fc17b72f72c1
andy <andy2472625@gmail.com>
2019-04-08 15:42:58 +0800

    style: modify phonecode list columns

--- src/components/containers/phonecode/Index.vue
+++ src/components/containers/phonecode/Index.vue
@@ -3,18 +3,20 @@
     .section__header
       h3.text-primary.text-center 申請手機條碼
     .section__main
-      button.phonecode__btn.columns(@click="onNextStep(true)")
-        .column.col-3.btn-icon
-          .icon.icon-yes
-        .column.col-8 我已有手機條碼
-        .column.col-1
-          i.arrow.arrow-right
-      button.phonecode__btn.columns(@click="onNextStep(false)")
-        .column.col-3.btn-icon
-          .icon.icon-no
-        .column.col-8 我沒有手機條碼
-        .column.col-1
-          i.arrow.arrow-right
+      button.phonecode__btn(@click="onNextStep(true)")
+        .columns
+          .column.col-3.btn-icon
+            .icon.icon-yes
+          .column.col-7 我已有手機條碼
+          .column.col-2
+            i.arrow.arrow-right
+      button.phonecode__btn(@click="onNextStep(false)")
+        .columns
+          .column.col-3.btn-icon
+            .icon.icon-no
+          .column.col-7 我沒有手機條碼
+          .column.col-2
+            i.arrow.arrow-right
       span.text-sm 發票手機條碼範例：/YLC＊CE7
 </template>
 
@@ -48,7 +50,10 @@ export default {
     padding: #{convertUnit(15)} #{convertUnit(20)};
     margin-bottom: convertUnit(15);
     font-size: $fontSize3;
-    align-items: center;
+
+    .columns {
+      align-items: center;
+    }
 
     .btn-icon {
       font-size: convertUnit(60);
